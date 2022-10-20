e5ad819e0fd1b02744e8966320a76c66207a9c3e
ayush9953 <77432436+ayush9953@users.noreply.github.com>
2022-10-20 22:32:44 +0530

    Update slice.js

--- src/reduxStore/slice.js
+++ src/reduxStore/slice.js
@@ -1,29 +1,23 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  data:null
-}
+  data: {},
+  error: false,
+};
 
 export const weatherSlice = createSlice({
-  name: 'weatherData',
+  name: "weatherData",
   initialState,
   reducers: {
-    
-      getDataSuccess: (state, { payload }) => {
-        state.data = payload
-        state.hasErrors=false;
-      }
-     
+    getDataSuccess: (state, { payload }) => {
+      state.data = payload;
+    },
   },
-})
+});
 
-// Action creators are generated for each case reducer function
-export const {  getDataSuccess} = weatherSlice.actions
+export const { getDataSuccess } = weatherSlice.actions;
 
 export const store = configureStore({
-  reducer: weatherSlice.reducer
-})
-store.subscribe(() => console.log(store.getState()))
-
-// export const dataSelector = state => state.data;
-
+  reducer: weatherSlice.reducer,
+});
+store.subscribe(() => console.log(store.getState()));
